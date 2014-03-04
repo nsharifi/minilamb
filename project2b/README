90f949a6b0c443239bf044cbe06207658379b130
Naser Sharifi <sharifinaser@gmail.com>
2014-03-03 20:40:19 -0600

    Clean things up

---
+++ project2b/README
@@ -0,0 +1,11 @@
+### Team 2
+David Tan, Naser Sharifi
+
+Comp 471 Programming Languages
+
+Dr Konstantin Läufer
+
+Project2b, March 3 2014
+
+[The Repository](https://trello.com/c/eLGw4sBD/32-project-2b "The Repository")
+For [the report](doc/report.md "The report") please refer to doc/report.md
---
+++ project2b/src/test/scala/lawTests.scala
@@ -0,0 +1,34 @@
+package edu.luc.cs.cs372.team2.shapeAlgebraic
+
+import org.scalatest.FunSuite
+import scalaz.syntax.equal._
+import scalaz.std.anyVal._
+import scalamu._
+/* this is an exploratory test learning in progress..NOT required for project2b*/
+class lawTests extends FunSuite {
+
+  import structures._
+  import structures.ShapeFactory._
+
+  test("equality works") {
+    (Ellipse(3, 2): ShapeF[Int]) assert_=== (Ellipse(3, 2): ShapeF[Int])
+    ellipse(3, 2) assert_=== ellipse(3, 2)
+//    (Location(10, 20, Ellipse(3, 2))) assert_=== (Location(10, 20, Ellipse(3, 2)))
+  }
+
+//  test("equality and functor laws hold for ExprF") {
+    import scalaz.syntax.functor._
+    import scalaz.scalacheck.ScalazArbitrary._
+    import scalaz.scalacheck.ScalaCheckBinding._
+    import scalaz.scalacheck.ScalazProperties._
+    import org.scalacheck.Arbitrary
+
+//    implicit def ShapeFArbitrary[A](implicit a: Arbitrary[A]): Arbitrary[ShapeF[A]] =
+//      a map { a => (Plus(a, a): ExprF[A]) }
+//
+//    // TODO figure out how to integrate these checks better with ScalaTest
+//    //  so that the test fails if one of the checks does
+//    equal.laws[ExprF[Int]].check
+//    functor.laws[ExprF].check
+//  }
+}
