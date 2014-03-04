f2cdd599a38036b43ef85051bd79035e5cf03e44
Naser Sharifi <sharifinaser@gmail.com>
2014-03-03 22:14:19 -0600

    Do Cleanup

--- project2b/src/test/scala/lawTests.scala
+++ project2b/src/test/scala/lawTests.scala
@@ -4,7 +4,6 @@ import org.scalatest.FunSuite
 import scalaz.syntax.equal._
 import scalaz.std.anyVal._
 import scalamu._
-/* this is an exploratory test learning in progress..NOT required for project2b*/
 class lawTests extends FunSuite {
 
   import structures._
@@ -12,23 +11,7 @@ class lawTests extends FunSuite {
 
   test("equality works") {
     (Ellipse(3, 2): ShapeF[Int]) assert_=== (Ellipse(3, 2): ShapeF[Int])
-    ellipse(3, 2) assert_=== ellipse(3, 2)
-//    (Location(10, 20, Ellipse(3, 2))) assert_=== (Location(10, 20, Ellipse(3, 2)))
   }
 
-//  test("equality and functor laws hold for ExprF") {
-    import scalaz.syntax.functor._
-    import scalaz.scalacheck.ScalazArbitrary._
-    import scalaz.scalacheck.ScalaCheckBinding._
-    import scalaz.scalacheck.ScalazProperties._
-    import org.scalacheck.Arbitrary
 
-//    implicit def ShapeFArbitrary[A](implicit a: Arbitrary[A]): Arbitrary[ShapeF[A]] =
-//      a map { a => (Plus(a, a): ExprF[A]) }
-//
-//    // TODO figure out how to integrate these checks better with ScalaTest
-//    //  so that the test fails if one of the checks does
-//    equal.laws[ExprF[Int]].check
-//    functor.laws[ExprF].check
-//  }
 }
