735643d6827a0e4b069f22bc21a9187a21abc944
davidtanloyola <devnull@localhost>
2014-03-25 15:18:54 -0500

    explore added

--- project3a/src/test/scala/fixtures3a.scala
+++ project3a/src/test/scala/fixtures3a.scala
@@ -4,23 +4,6 @@ import structures.ExprFactory._
 
 object fixtures3a {
 
-  import scalamu.ToMuOps
-  import behaviors._
 
-  eval(Var("x")) -> error
-  eval(Fun("x", Plus(Constant(7), Var("x"))))
-  -> Fun("x", Plus(Const(7), Var("x")))
-  eval(App(Fun("x", Plus(Const(7), Var("x"))), Const(3)))
-  -> Const(10)
-  eval(App(Var("x"), Const(3)) -> error
-    eval(If(Const(7), Const(3), Const(4))) -> Const(3)
-    eval(If(Const(0), Const(3), Const(4))) -> Const(4)
-    eval(If(Fun("x", Var("x")), Const(3), Const(4))) -> Const(3)
-    eval(If(Fun("x", Var("y")), Const(3), Const(4))) -> Const(3)
-  val Y = // you'll define it yourself as in the handout!
-    eval(App(App(Y, Fun("f", Fun("n", If(Var("n"),
-      Times(Var("n"), App(Var("f"), Minus(Var("n"), Const(1)))), Const(1))))),
-      Const(5)))
-  -> Const(120)
 
 }
--- project3a/target/scala-2.10/classes/Xplore.sc
+++ project3a/target/scala-2.10/classes/Xplore.sc
@@ -1,4 +1,20 @@
-/**
- * Created by David on 3/25/14.
- */
 println("@")
+
+import scalamu.ToMuOps
+import project3a.behaviors._
+
+eval(Var("x")) -> error
+eval(Fun("x", Plus(Constant(7), Var("x"))))
+-> Fun("x", Plus(Const(7), Var("x")))
+eval(App(Fun("x", Plus(Const(7), Var("x"))), Const(3)))
+-> Const(10)
+eval(App(Var("x"), Const(3)) -> error
+  eval(If(Const(7), Const(3), Const(4))) -> Const(3)
+  eval(If(Const(0), Const(3), Const(4))) -> Const(4)
+  eval(If(Fun("x", Var("x")), Const(3), Const(4))) -> Const(3)
+  eval(If(Fun("x", Var("y")), Const(3), Const(4))) -> Const(3)
+val Y = // you'll define it yourself as in the handout!
+  eval(App(App(Y, Fun("f", Fun("n", If(Var("n"),
+    Times(Var("n"), App(Var("f"), Minus(Var("n"), Const(1)))), Const(1))))),
+    Const(5)))
+-> Const(120)
