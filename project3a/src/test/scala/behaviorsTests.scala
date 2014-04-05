c260040e1b752b04404a26de27edaa3c13c24933
Naser Sharifi <sharifinaser@gmail.com>
2014-04-04 23:03:01 -0500

    Do refactoring

--- project3a/src/test/scala/behaviorsTests.scala
+++ project3a/src/test/scala/behaviorsTests.scala
@@ -35,21 +35,32 @@ class behaviorTests extends FunSuite {
 
   test("interpret works") {
     assert(interpret(plus(constant(2), constant(3))) == constant(5))
-    assert(interpret(constant(3)) == In(Constant(3)))
-    assert(interpret(variable("x")) == In(Error("Variable")))
-    assert(interpret(fun("x", plus(constant(7), variable("x")))) == In(Fun("x", In(Plus(In(Constant(7)), In(Var("x")))))))
-    assert(interpret(app(variable("x"), constant(3))) == In(Error("Var Application")))
+    assert(interpret(constant(3)) == constant(3))
+    assert(interpret(variable("x")) == err("Variable"))
+    assert(interpret(fun(variable("x"), plus(constant(7), variable("x")))) ==
+      fun(variable("x"), plus(constant(7), variable("x"))))
+    assert(interpret(app(variable("x"), constant(3))) == err("Var Application"))
     //assert(interpret(app(fun("x", plus(constant(7), variable("x"))), constant(3))) == In(Constant(10)))
     //B
-    assert(interpret( iff(constant(7), constant(3), constant(4)) ) == In(Constant(3)))
-    assert(interpret(iff(constant(0), constant(3), constant(4)) ) == In(Constant(4)))
-    assert(interpret(iff(variable("x"), constant(3), constant(4)) ) == In(Error("Var Conditional")))
+    assert(interpret( iff(constant(7), constant(3), constant(4)) ) == constant(3))
+    assert(interpret(iff(constant(0), constant(3), constant(4)) ) == constant(4))
+    assert(interpret(iff(variable("x"), constant(3), constant(4)) ) == err("Var Conditional"))
    // assert(interpret(iff(fun("x", variable("x")), constant(3), constant(4)) ) == In(Constant(4)))
-    assert(interpret(fun("x", plus(constant(7), variable("x")))) == In(Fun("x", In(Plus(In(Constant(7)), In(Var("x")))))))
-    assert(interpret(app(variable("x"), constant(3))) == In(Error("Var Application")))
-    assert(interpret(app(fun("x", plus(constant(7), variable("x"))), constant(3))) == In(Constant(10)))
+    assert(interpret(fun(variable("x"), plus(constant(7), variable("x")))) ==
+      fun(variable("x"), plus(constant(7), variable("x"))))
+    assert(interpret(app(variable("x"), constant(3))) == err("Var Application"))
+//    assert(interpret(app(fun("x", plus(constant(7), variable("x"))), constant(3))) == constant(10))
   }
+  test("reduce works") {
+    assert (reduce(variable("x"), plus(constant(3), constant(4)), variable("x")) == plus(constant(3), constant(4)))
+    assert (reduce(variable("y"), plus(constant(1), constant(2)), variable("x")) == plus(constant(1), constant(2)))
+    assert (reduce(variable("y"), fun(variable("z"), plus(constant(1), constant(2))), variable("x")) ==
+      fun(variable("z"), plus(constant(1), constant(2))))
+    assert (reduce(constant(10), plus(constant(1), constant(2)), variable("x")) == constant(10))
+    assert (reduce(fun(variable("y"), constant(3)), constant(5), variable("y")) == fun(variable("y"), constant(3)))
+    assert (reduce(fun(variable("y"), constant(3)), constant(5), variable("x")) != fun(variable("y"), constant(3)))
 
+  }
 
 //  test("evaluate works") {
 //    fixtures.complex1 cata evaluate assert_=== -1
